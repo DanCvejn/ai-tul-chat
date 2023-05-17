7c2ff516235c69e6d12a0b81bbbfc38aae14f98f
DanCvejn <70745296+DanCvejn@users.noreply.github.com>
2023-05-17 21:08:02 +0200

    new app

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const HomePage = () => {
+  const [weather, setWeather] = useState(null);
+
+  const getRes = async () => {
+    const res = await axios.get(import.meta.env.VITE_API_URL + "/current.json", {
+      params: {
+        key: import.meta.env.VITE_API_KEY,
+        q: "Liberec",
+        lang: "cs"
+      }
+    });
+    return res.data.current;
+  }
+
+  useEffect(() => {
+    const data = getRes();
+    console.log(data);
+    setWeather(data);
+  }, []);
+
   return (
-    <div>AI CHAT TUL</div>
+    <>
+      {weather ?
+        <div>
+          <h3>{weather?.condition?.text}</h3>
+          <img src={"https:" + weather?.condition?.icon} alt="" />
+        </div> :
+        <h1>Načítání</h1>
+      }
+    </>
   )
 }
