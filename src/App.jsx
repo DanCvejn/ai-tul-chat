0dad15b6c864231764119eb1170883e727ddb4cc
DanCvejn <70745296+DanCvejn@users.noreply.github.com>
2023-05-22 20:32:00 +0200

    searchUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./assets/style.scss";
+import { IconLoader2, IconSearch } from "@tabler/icons-react";
 
-const getData = async (setData, type, lat, lng) => {
-  // const ip = await axios.get("https://api.ipify.org?format=json");
-  const res = await axios.get(import.meta.env.VITE_API_URL + "/" + type + ".json", {
+const getData = async (setData, type, lat, lng, city, setLoading) => {
+  setLoading(true);
+  if (city) {
+    window.localStorage.setItem("city", city);
+  }
+  await axios.get(import.meta.env.VITE_API_URL + "/" + type + ".json", {
     params: {
       key: import.meta.env.VITE_API_KEY,
-      q: lat + "," + lng,
-      // q: ip.data.ip,
+      q: (!city || city === "") ? lat + "," + lng : city,
       lang: "cs",
     }
-  });
-  if (setData) return setData(res.data);
-  return res.data;
+  })
+    .then((res) => {
+      setLoading(false);
+      if (setData) return setData(res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      setLoading(false);
+      console.log(err);
+      setData({ error: true });
+    });
 }
 
 const images = [
@@ -100,6 +111,9 @@ const windDirections = {
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [citySearch, setCitySearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [searchTab, setSearchTab] = useState(false);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -107,7 +121,8 @@ function App() {
         function(position) {
           const lat = position.coords.latitude;
           const lng = position.coords.longitude;
-          getData(setWeather, "current", lat, lng);
+          const city = window.localStorage.getItem("city");
+          getData(setWeather, "current", lat, lng, city, setLoading);
         },
         function(error) {
           console.error("Error getting user location:", error);
@@ -117,6 +132,10 @@ function App() {
       console.error("Geolocation is not supported by this browser.");
     }
   }, []);
+
+  useEffect(() => {
+    if (!weather?.error) setCitySearch(weather?.location.name);
+  }, [weather])
 
   const getImage = () => {
     let selected = images.find(image => { return image.name.includes(weather?.current.condition.text) });
@@ -136,47 +155,87 @@ function App() {
 
   return (
     <>
-      {weather ?
-        <div className="card">
-          <div className="card__topInfo">
-            <p className="city">{weather?.location.name}</p>
-            <p className="time">{weather?.location.localtime.split(" ")[1]}</p>
-          </div>
-          <div className="card__mainInfo">
-            <img
-              src={getImage()}
-              alt=""
-            />
-            <h3>{weather?.current.condition.text}</h3>
-          </div>
-          <div className="card__bottomInfo">
-            <div>
-              <div className="line">
-                <img src="./img/humidity.svg" alt="Rychlost větru" />
-                <p>{weather?.current.humidity}%</p>
-              </div>
-              <div className="line">
-                <img src="./img/wind.svg" alt="Rychlost větru" />
-                <p>{weather?.current.wind_kph} km/h</p>
-              </div>
-              <div className="line">
-                <img src="./img/compass.svg" alt="Rychlost větru" />
-                <p>{getDirection()}</p>
-              </div>
-            </div>
-            <div>
-              <p className="temperature">
-                {weather?.current.temp_c}
-              </p>
-              <p className="feelslike">
-                Pocitová: {weather?.current.feelslike_c} °C
-              </p>
-            </div>
-          </div>
+      {loading ?
+        <div className="card loading">
+          <IconLoader2 size={48} stroke={1.5} />
         </div> :
-        <div className="card not-available">
-          <img src="./img/not-available.svg" alt="načítání" />
-        </div>
+        <>
+          {(weather && !weather?.error) ?
+            <div className="card">
+              {searchTab &&
+                <>
+                  <div className="overlay" onClick={() => setSearchTab(false)}>
+                  </div>
+                  <div className="searchBar">
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        getData(setWeather, "current", null, null, citySearch, setLoading);
+                        setSearchTab(false)
+                      }}
+                    >
+                      <input
+                        type="text"
+                        value={citySearch}
+                        onChange={(e) => setCitySearch(e.target.value)}
+                        disabled={loading}
+                        placeholder="Zadejte město"
+                      />
+                      <button
+                        type="submit"
+                        disabled={citySearch.length < 4}
+                      >
+                        <IconSearch size={24} stroke={1.5} />
+                      </button>
+                    </form>
+                  </div>
+                </>
+              }
+              <div className="card__topInfo">
+                <div className="city" onClick={() => setSearchTab(true)}>
+                  <p>
+                    {weather?.location.name}
+                  </p>
+                  <IconSearch size={16} stroke={1.5} /></div>
+                <p className="time">{weather?.location.localtime.split(" ")[1]}</p>
+              </div>
+              <div className="card__mainInfo">
+                <img
+                  src={getImage()}
+                  alt=""
+                />
+                <h3>{weather?.current.condition.text}</h3>
+              </div>
+              <div className="card__bottomInfo">
+                <div>
+                  <div className="line">
+                    <img src="./img/humidity.svg" alt="Rychlost větru" />
+                    <p>{weather?.current.humidity}%</p>
+                  </div>
+                  <div className="line">
+                    <img src="./img/wind.svg" alt="Rychlost větru" />
+                    <p>{weather?.current.wind_kph} km/h</p>
+                  </div>
+                  <div className="line">
+                    <img src="./img/compass.svg" alt="Rychlost větru" />
+                    <p>{getDirection()}</p>
+                  </div>
+                </div>
+                <div>
+                  <p className="temperature">
+                    {weather?.current.temp_c}
+                  </p>
+                  <p className="feelslike">
+                    Pocitová: {weather?.current.feelslike_c} °C
+                  </p>
+                </div>
+              </div>
+            </div> :
+            <div className="card not-available">
+              <img src="./img/not-available.svg" alt="načítání" />
+            </div>
+          }
+        </>
       }
     </>
   )
